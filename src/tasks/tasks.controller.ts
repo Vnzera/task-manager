4abb8ce4d2e1335ed0dc0246d73fe0e3c06a5b8e
Luis Diaz <luis@linuxmail.org>
2019-07-24 09:46:11 -0700

    Added route logic for finding a specific task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,7 +13,14 @@ export class TasksController {
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
     }
-
+    // /:id is a url parameter that the front end must provide in the request
+    // @Param tells NestJS to look for the 'id' parameter in the request
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+    // @Body and createTaskDto work together here
+    // NestJS searches the @Body of the request for Dto data ie 'title' 'description'
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
