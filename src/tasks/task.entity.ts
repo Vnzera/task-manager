5404b0bb4674aed5cc5e477b02f643b658037180
Luis Diaz <luis@linuxmail.org>
2019-08-26 00:11:36 -0700

    Beginning to setup Auth

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 // this entity describes the structure of our Tasks as it pertains to postgres
-// similar to how Task model defines the structure pertaining to req/res data
+// similar to how Task model/interface defines the structure pertaining to req/res data
 
 @Entity()
 export class Task extends BaseEntity {
